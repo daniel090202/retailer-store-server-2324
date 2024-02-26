cc36747b98a68b50383f1d01a4b1363a3d725b98
Daniel Nguyen <minhkhanh090202@gmail.com>
2024-02-27 00:10:06 +0700

    update-tsconfig.json

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,11 +2,11 @@ import { Module } from '@nestjs/common';
 import { HttpModule } from '@nestjs/axios';
 import { ConfigModule } from '@nestjs/config';
 
-import { AuthModule } from '@/auth/auth.module';
-import { PrismaModule } from '@/prisma/prisma.module';
-
 import { AppService } from './app.service';
 import { AppController } from './app.controller';
+
+import { AuthModule } from './auth/auth.module';
+import { PrismaModule } from './prisma/prisma.module';
 
 import { UsersModule } from './users/users.module';
 import { UsersService } from './users/users.service';
